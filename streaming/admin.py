3b709e70bcbd73c5ab759c7e2a6148a767c92da4
delphee <d_elphee@yahoo.com>
2025-10-26 17:09:00 -0400

    First

--- streaming/admin.py
+++ streaming/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+admin.site.register(Conversation)
+admin.site.register(Speaker)
+admin.site.register(TranscriptSegment)
+admin.site.register(ConversationAnalysis)
+admin.site.register(UserProfile)
+admin.site.register()
