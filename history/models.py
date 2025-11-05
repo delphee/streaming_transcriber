6a2a238d1a800026307b457a48f2ef71a32572a1
delphee <d_elphee@yahoo.com>
2025-11-05 16:20:29 -0500

    history models

--- history/models.py
+++ history/models.py
@@ -6,3 +6,28 @@ from django.db import models
 class AccessToken(models.Model):
     token =models.TextField()
     when = models.DateTimeField()
+
+
+class DispatchJob(models.Model):
+    job_id = models.CharField(max_length=50)
+    appointment_id = models.CharField(max_length=50)
+    tech_id = models.CharField(max_length=50)
+    active = models.BooleanField(default=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    polling_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.job_id} - {self.appointment_id} {'Polling...' if self.polling_active else ''}"
+
+
+class HistoryJob(models.Model):
+    job_id = models.CharField(max_length=50)
+    appointment_id = models.CharField(max_length=50)
+    ready = models.BooleanField(default=False)
+    data = models.TextField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.job_id} - {self.appointment_id} {'Ready' if self.ready and self.active else ''}"
+
+
