c44ef5569915d414df474e94466738a350c578ff
delphee <d_elphee@yahoo.com>
2025-11-06 17:29:31 -0500

    history models

--- history/tasks.py
+++ history/tasks.py
@@ -11,7 +11,7 @@ def pollA():
         print("pollA...")
         dispatch_jobs = DispatchJob.objects.filter(active=True)
         for d_job in dispatch_jobs:
-            jobs = jobs_api_call(jobNumber=d_job.id)
+            jobs = jobs_api_call(jobNumber=d_job.job_id)
             if len(jobs)==0:
                 # Job was deleted
                 d_job.delete()
