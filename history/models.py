555b719900c861cf3e51c59ca07dfd0531bce84e
delphee <d_elphee@yahoo.com>
2025-11-10 17:48:53 -0500

    login changes

--- history/models.py
+++ history/models.py
@@ -24,7 +24,8 @@ TECHS = {
     "133853401":"Stephen Starner",
     "67321105":"Thomas Shawaryn",
     "114376585":"Tim Miller",
-    "125325480":"Jake Simpson"
+    "125325480":"Jake Simpson",
+    "111119391":"Dewayne McCauley"
 }
 
 
